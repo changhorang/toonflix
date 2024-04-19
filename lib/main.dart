0f548f91f6d2a1d3e6911b846d22e80e1ee397e5
changhorang <rlackdgh124@gmail.com>
2024-04-19 18:07:14 +0900

    [20240419] Widget, Header

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,31 @@ class App extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              centerTitle: true,
-              title: Text('Hello Flutter!'),
-            ),
-            body: Center(
-              child: Text('Hello World!'),
-            )));
+            backgroundColor: Color(0xFF181818),
+            body: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    SizedBox(
+                      height: 80,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                    ),
+                    Text(
+                      'Hey, Seul Gi',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 38,
+                          fontWeight: FontWeight.w800),
+                    ),
+                    Text(
+                      'Welcome back',
+                      style: TextStyle(
+                          color: Colors.white.withOpacity(0.7), fontSize: 22),
+                    ),
+                  ],
+                ))));
   }
 }
