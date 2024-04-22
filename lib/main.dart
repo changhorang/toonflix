a847d5dc4210f836b67848048e34e0d810a0df8b
changhorang <rlackdgh124@gmail.com>
2024-04-22 23:15:51 +0900

    [Button Sections]

--- lib/main.dart
+++ lib/main.dart
@@ -14,26 +14,73 @@ class App extends StatelessWidget {
             body: Padding(
                 padding: EdgeInsets.symmetric(horizontal: 40),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     SizedBox(
                       height: 80,
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              'Hey, Seul Gi',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 38,
+                                  fontWeight: FontWeight.w800),
+                            ),
+                            Text(
+                              'Welcome back',
+                              style: TextStyle(
+                                  color: Colors.white.withOpacity(0.7),
+                                  fontSize: 21),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 120,
                     ),
                     Text(
-                      'Hey, Seul Gi',
+                      'Total Balance',
                       style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 38,
-                          fontWeight: FontWeight.w800),
+                          color: Colors.white.withOpacity(0.8), fontSize: 22),
+                    ),
+                    SizedBox(
+                      height: 20,
                     ),
                     Text(
-                      'Welcome back',
+                      '\$5 194 482',
                       style: TextStyle(
-                          color: Colors.white.withOpacity(0.7), fontSize: 22),
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 42,
+                          fontWeight: FontWeight.w600),
                     ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius: BorderRadius.circular(45)),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 40,
+                            ),
+                            child: Text(
+                              'Transfer',
+                              style: TextStyle(fontSize: 22),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
                   ],
                 ))));
   }
